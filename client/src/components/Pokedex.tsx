51eca653db8d82aaa2eb6a92248c81d141c671ad
Oumar3 <oumaralitori1@gmail.com>
2025-10-20 16:10:06 +0100

    feat: fetch all pokemon

--- client/src/components/Pokedex.tsx
+++ client/src/components/Pokedex.tsx
@@ -38,11 +38,11 @@ const Pokedex: React.FC<Pokemon> = () => {
     <div>
       <h2>Liste des Pokémons</h2>
       <ul className="pokemon-list">
-        {pokemons.length > 0 ? pokemons.map((pokemon) => (
-          <li key={pokemon._id} className="pokemon-item">
-            <h3>{pokemon.name} (#{pokemon.number})</h3>
-            <img src={pokemon.imageUrl} alt={pokemon.name} className="pokemon-image" />
-            <a href={`${import.meta.env.endpoints}/${pokemon._id}`}>Voir détails</a>
+        {pokemons.length > 0 ? pokemons.map((pokemon,index) => (
+          <li key={index} className="pokemon-list-item">
+            <h3>{pokemon.name}</h3>
+            {pokemon.imageUrl && <img src={pokemon.imageUrl} alt="Photo" />}
+            <a href={`${import.meta.env.VITE_API_URL}/pokemons/${pokemon._id}`}>Voir détails</a>
           </li>
         )) : <p>pas de données sur les Pokémons pour le moment</p>}
       </ul>
